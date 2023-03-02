b228fc6f598917bc9e6c3b85d973ec99c4537659
Sarah Toukour <sarahtoukour@gmail.com>
2023-03-02 17:23:15 +0100

    ignore

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const express = require('express');
-
-const app = express();
-
-module.exports = app;
-
 const mongoose = require('mongoose');
 
 mongoose
   .connect('mongodb+srv://sarahtoukour:openMongo@cluster0.vkhxbq5.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+const userRoutes = require('./routes/user');
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use(express.json());
+app.use('/api/auth', userRoutes);
+
+module.exports = app;
