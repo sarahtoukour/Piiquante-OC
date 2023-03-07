63df2cd596c96c32fb03e4f792dbee7dde13a244
Sarah Toukour <sarahtoukour@gmail.com>
2023-03-07 09:33:42 +0100

    essai put et delete

--- routes/sauce.js
+++ routes/sauce.js
@@ -3,8 +3,12 @@ const router = express.Router();
 const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
 
-const sauceCtrl = require('../controllers/sauce ');
+const sauceCtrl = require('../controllers/sauce');
 
 router.post('/', auth, multer, sauceCtrl.createSauce);
+router.put('/:id', auth, multer, sauceCtrl.modifySauce);
+router.delete('/:id', auth, sauceCtrl.deleteSauce);
+router.get('/', auth, sauceCtrl.getAllSauces);
+router.get('/:id', auth, sauceCtrl.getOneSauce);
 
 module.exports = router;
