bbafdf2c541ae61b7a2ccb5796315b609f406de6
Sarah Toukour <sarahtoukour@gmail.com>
2023-03-07 10:54:13 +0100

    modif erreur user

--- controllers/user.js
+++ controllers/user.js
@@ -33,7 +33,7 @@ exports.login = (req, res, next) => {
               res.status(401).json({ message: 'Paire identifiant/mot de passe incorrecte' });
             } else {
               res.status(200).json({
-                iserId: user._id,
+                userId: user._id,
                 token: jwt.sign({ userId: user._id }, 'RANDOM_TOKEN_SECRET', { expiresIn: '24h' }),
               });
             }
